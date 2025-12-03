160827f00695daa552786d710f5d77bf9677944c
enojen <enesozel.eo@gmail.com>
2025-12-03 12:15:19 +0300

    fix OpenAPI description

--- src/main/java/com/online/grocery/pricing/config/OpenApiConfiguration.java
+++ src/main/java/com/online/grocery/pricing/config/OpenApiConfiguration.java
@@ -1,9 +1,7 @@
 package com.online.grocery.pricing.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,27 +35,23 @@ public class OpenApiConfiguration {
                                 ### Bread
                                 - Base price: €1.00 per unit
                                 - 0-2 days old: No discount
-                                - 3-5 days old: "Buy 1 take 2" (50% off in groups of 2)
-                                - 6 days old: "Buy 1 take 3" (66% off in groups of 3)
+                                - 3 days exactly: "Buy 1 take 2" (50% off in groups of 2)
+                                - 4-5 days old: No discount
+                                - 6 days exactly: "Pay 1 take 3" (66% off in groups of 3)
+                                - >6 days: Not allowed
                                                    \s
                                 ### Vegetables
                                 - Base price: €1.00 per 100g
                                 - 0-99g: 5% discount
-                                - 100-499g: 7% discount
-                                - 500g+: 10% discount
+                                - 100-500g: 7% discount
+                                - >500g: 10% discount
                                                    \s
                                 ### Beer
                                 - Belgian: €0.60/bottle, €3.00 pack discount
                                 - Dutch: €0.50/bottle, €2.00 pack discount
                                 - German: €0.80/bottle, €4.00 pack discount
                                 - Pack = 6 bottles
-                               \s""")
-                        .contact(new Contact()
-                                .name("API Support")
-                                .email("support@example.com"))
-                        .license(new License()
-                                .name("MIT License")
-                                .url("https://opensource.org/licenses/MIT")))
+                               \s"""))
                 .servers(List.of(
                         new Server()
                                 .url("http://localhost:8080")
